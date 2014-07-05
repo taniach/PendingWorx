e6b8183583ca24a13112b5d192b84583ee9ef988
Tania Chattopadhyay <tchatterjee5@gmail.com>
2014-07-05 16:35:37 +0800

    Add main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+import urllib
+
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+import webapp2
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + '/templates'),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+application = webapp2.WSGIApplication([
+    ('/', MainPage),
+    ('/write', Group),
+], debug=True)
